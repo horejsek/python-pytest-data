d3418009da05898743e3563b2a0e8ef31ded6389
Michal Horejsek <horejsekmichal@gmail.com>
2019-09-08 20:58:13 +0200

    Fix generating docs

--- docs/conf.py
+++ docs/conf.py
@@ -118,7 +118,7 @@ html_theme = 'nature'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -322,6 +322,6 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx ----------------------------------------------
 
 intersphinx_mapping = {
-    'python': ('http://docs.python.org/2.7', None),
-    'pytest': ('http://pytest.org/latest/', None),
+    'python': ('https://docs.python.org/3.7', None),
+    'pytest': ('https://docs.pytest.org/en/latest', None),
 }
